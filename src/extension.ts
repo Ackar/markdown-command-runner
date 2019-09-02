b355414ade137790f2adc774a696ddba0eb211c9
Sylvain Cleymans <sylvain.cleymans@gmail.com>
2019-09-02 21:43:25 +1200

    Create a new terminal if there's a command running the active one

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,30 @@
 import * as vscode from 'vscode';
 import { CommandCodeLensProvider } from './commandCodeLensProvider';
+import cp = require('child_process');
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('markdown.run.command', (args) => {
-			const term = vscode.window.activeTerminal || vscode.window.createTerminal();
-			if (term) {
-				term.show();
-				term.sendText(args.command);
-			}
+			var term = vscode.window.activeTerminal || vscode.window.createTerminal();
+			// check if there's a running command in the active terminal, if there is one
+			// create a new term
+			term.processId.then(pid => {
+				cp.exec('ps -o state= -p ' + pid, (error, stdout, stderr) => {
+					if (error) {
+						// if we can't check just send to the current one...
+						term.show();
+						term.sendText(args.command);
+						return;
+					}
+
+					// a + in the state indicates a process running in foreground
+					if (!stdout.includes('+')) {
+						term = vscode.window.createTerminal();
+					}
+					term.show();
+					term.sendText(args.command);
+				});
+			});
 		})
 	);
 
